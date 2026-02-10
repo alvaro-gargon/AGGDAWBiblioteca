39dc5264e5c44453ba8de73258699296a9aaddb3
alvaro-gargon <alvaro.garcia.daw@gmail.com>
2026-02-10 09:37:20 +0100

    feat - config update

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,8 @@
 <?php
-const base_url = "http://localhost/biblio/";
+const base_url = "http://10.199.9.114/AGGDAWBiblioteca/";
 const host = "localhost";
-const user = "root";
-const pass = "";
-const db = "biblioteca";
+const user = "userDBBiblioteca";
+const pass = "paso";
+const db = "DBBiblioteca";
 const charset = "charset=utf8";
 ?>
